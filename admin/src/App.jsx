3fab7478af95db1b96d10b2e205e98927ca9db76
WaleedAkram01 <waleedakramwaleedakram592@gmail.com>
2026-06-16 17:03:07 +0500

    upated chnaages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import Navbar from './components/Navbar.jsx'
+import SideBar from './components/Sidebar.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
 function App() {
   return (
-    <div>
-      <h1>Admin Panel</h1>
+    <div className='min-h-screen bg-gray-50'>
+      <>
+        <Navbar />
+        <hr />
+        <div className='flex w-full'>
+
+          <SideBar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base ' >
+            <Routes>
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      </>
     </div>
   )
 }

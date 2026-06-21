9c9d563db669b4d94eb07d28d356531967db7e37
WaleedAkram01 <waleedakramwaleedakram592@gmail.com>
2026-06-22 00:12:16 +0500

    changes in placeOrder page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -5,32 +5,59 @@ import CartTotal from '../components/CartTotal'
 import { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 function PlaceOrder() {
     const [method, setMethod] = useState('cod')
+    const { navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products } = useContext(ShopContext);
+
+    // Purpose of this state variable is to store the form data that user will enter in the delivery information form. It will be an object with keys as the form field names and values as the form field values.
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        street: '',
+        city: '',
+        state: '',
+        zipcode: '',
+        country: '',
+        phone: ''
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+
+        setFormData(data => ({ ...data, [name]: value }))
+    }
+
     const { navigate } = useContext(ShopContext)
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+
+    }
     return (
-        <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5  min-h-[80vh] border-t '>
+        <form onSubmit={onSubmitHandler} className='flex flex-col sm:flex-row justify-between gap-4 pt-5  min-h-[80vh] border-t '>
             <div className='flex flex-col gap-4 w-full sm:w-[480px]'>
                 <div className='text-xl sm:text-2xl my-3'>
                     <Title text1={'DELIVERY'} text2={'INFORMATION'} />
                 </div>
                 <div className='flex gap-3'>
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='First name' />
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Last name' />
+                    <input required onChange={onChangeHandler} name='firstName' value={formData.firstName} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='First name' />
+                    <input required onChange={onChangeHandler} name='lastName' value={formData.lastName} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Last name' />
                 </div>
-                <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='email' placeholder='Email address' />
-                <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Street' />
+                <input required onChange={onChangeHandler} name='email' value={formData.email} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='email' placeholder='Email address' />
+                <input required onChange={onChangeHandler} name='street' value={formData.street} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Street' />
                 <div className='flex gap-3'>
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='City' />
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='State' />
+                    <input required onChange={onChangeHandler} name='city' value={formData.city} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='City' />
+                    <input required onChange={onChangeHandler} name='state' value={formData.state} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='State' />
                 </div>
                 <div className='flex gap-3'>
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='number' placeholder='Zipcode' />
-                    <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Country' />
+                    <input required onChange={onChangeHandler} name='zipcode' value={formData.zipcode} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='number' placeholder='Zipcode' />
+                    <input required onChange={onChangeHandler} name='country' value={formData.country} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='text' placeholder='Country' />
                 </div>
 
-                <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='number' placeholder='Phone ' />
+                <input required onChange={onChangeHandler} name='phone' value={formData.phone} className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type='number' placeholder='Phone ' />
 
 
             </div>
@@ -64,7 +91,7 @@ function PlaceOrder() {
 
 
                     <div className='w-full text-end mt-8'>
-                        <button onClick={() => navigate('/orders')} className='bg-black text-white text-sm my-8 px-8 py-3'>
+                        <button type='submit' className='bg-black text-white text-sm my-8 px-8 py-3'>
                             PLACE ORDER
                         </button>
                     </div>
@@ -73,7 +100,7 @@ function PlaceOrder() {
 
             </div>
 
-        </div>
+        </form>
     )
 }
 

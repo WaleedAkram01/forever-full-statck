4a8911c65a5e400467a91ae409228f661e25b66a
WaleedAkram01 <waleedakramwaleedakram592@gmail.com>
2026-06-22 19:26:26 +0500

    stripe payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,12 @@
 import { orderModel } from "../models/orderModel.js";
 import { userModel } from "../models/userModel.js";
+import Stripe from "stripe";
+
+// global variables for stripe payment
+const currency = "PKR";
+const deliveryCharge = 10;
+//gateway initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing orders using COD Method
 // Agrr user nyy paymnet method mai COD select kia hai tou uss case mai yeh API call hogi aur order place hoga.
@@ -33,7 +40,61 @@ const placeOrder = async (req, res) => {
 };
 
 // Placing orders using Stripe Method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    // This userId is from token decode. And items, amount, address are from request body that user will send when placing order.
+    const { userId, items, amount, address } = req.body;
+    // Yeh origin basically na kya hai kyy na user ka domain name hai jahan syy user nyy request bheji hai. Agrr user nyy apne website syy request bheji hai tou origin mai uska domain name hoga aur agrr user nyy postman syy request
+    // Like abhii frontend syy basicalay krty  tou origin mai localhost:5173 aay ga.
+    const { origin } = req.headers;
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+    // Ab hum nyy orderData create kr diya hai tou usko database mai save kr den gy.
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    // After placing the order we would create line items using that we can execue stripe payment
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: deliveryCharge * 100,
+      },
+      quantity: 1,
+    });
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Placing orders using Razorpay Method
 const placeOrderRazorpay = async (req, res) => {};
